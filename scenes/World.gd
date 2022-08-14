cb907d1d9e36662fc78e0c8d403078c7578a2a44
maxhol23 <100097037+maxhol23@users.noreply.github.com>
2022-08-15 09:45:02 +1200

    Editing
    Minor edits to make game smoother

---
+++ scenes/World.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+func _ready():
+	pass
+	
+func _process(delta):
+	pass
+	if Input.is_action_just_pressed("reset"):
+		GameStats.reset()
+	$CanvasLayer/Control/Label.text = GameStats.get_time()
+
+
+
+
+func _on_Area2D_body_entered(body):
+	pass # Replace with function body.
+
+
